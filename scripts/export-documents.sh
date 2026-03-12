2337c3575c72d0d2465dd19e3889311855797937
Byron Chan <bschan@UCSD.EDU>
2026-03-11 19:06:03 -0700

    fixed bom dnp's

--- scripts/export-documents.sh
+++ scripts/export-documents.sh
@@ -10,7 +10,7 @@ SCHEMATIC_PATH="${PROJECT_PATH}.kicad_sch"
 PCB_PATH="${PROJECT_PATH}.kicad_pcb"
 
 # TODO: would be nice to make it do more than 1 project
-# PDFs are good for a single file for reading, SVGs better for big fat documentation
+# PDFs are good for a single file for reading, SVGs better for larger documentation
 
 # Documentation essentials: schematic & pcb PDFs
 kicad-cli sch export pdf $SCHEMATIC_PATH -o "${EXPORT_PATH}/schematics.pdf"
@@ -24,8 +24,8 @@ kicad-cli sch export svg $SCHEMATIC_PATH -o "$SVG_PATH"
 kicad-cli pcb export svg $PCB_PATH -o "${SVG_PATH}/pcb.svg" --mode-single -l $EXPORT_LAYERS # Can split to mutliple pages using --mode-multi
 
 # Production: BOM, gerbers, drill files
-kicad-cli sch export bom $SCHEMATIC_PATH -o "${EXPORT_PATH}/bom.csv" --group-by "Part Number,Reference,Value" \
---ref-range-delimiter --fields "Reference,Value,Footprint,$\{QUANTITY},$\{DNP},URL"
+kicad-cli sch export bom $SCHEMATIC_PATH -o "${EXPORT_PATH}/bom.csv" --group-by "Part Number,Reference,Value,\${DNP}" \
+--ref-range-delimiter --fields "Reference,Value,Part Number,Footprint,\${DNP}"
 kicad-cli pcb export gerbers $PCB_PATH -o "${EXPORT_PATH}/gerbers" --board-plot-params
 kicad-cli pcb export drill $PCB_PATH -o "${EXPORT_PATH}/gerbers" --excellon-separate-th
 
